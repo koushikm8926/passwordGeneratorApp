eee13cbac46bd416558c24c4489499176f0b418e
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-31 11:59:15 +0530

    yup form validation added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as Yup from 'yup'
+
+
+const passwordSchema= Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4,"should be minimum of 4 character")
+  .max(16,"should be maximum of 16 character")
+  .required("length is required")
+})
 
 // create a component
 const MyComponent = () => {
